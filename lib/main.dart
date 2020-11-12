bb3d7435fa67886e4033c820b2c0721c6f5ab1ef
wenbin.zhou@clife.cn <wenbin.zhou@clife.cn>
2020-11-12 20:46:47 +0800

    测试

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,6 @@ class SplashPage extends StatelessWidget {
 class SplashHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       body: Center(
         child: InkWell(
